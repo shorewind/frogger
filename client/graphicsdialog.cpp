932172d27cdb2c53f6bef47484204a89652f819e
Foti <ghf0004@uah.edu>
2024-11-18 17:46:55 -0600

    Revert "only sync obstacles on new client join"
    This reverts commit cbe29a44f6168df749ecaa56f7652c875ed880e1.

--- client/graphicsdialog.cpp
+++ client/graphicsdialog.cpp
@@ -84,8 +84,6 @@ GraphicsDialog::GraphicsDialog(QWidget *parent, QUdpSocket *socket) :
     createObstacle(Obstacle::Log, SCENE_WIDTH / 2 - 100, -197, -1, false);
     createObstacle(Obstacle::Log, SCENE_WIDTH / 2 + 100, -197, -1, false);
 
-    sendObstaclePositions();
-
     QTimer *collisionTimer = new QTimer(this);
     connect(collisionTimer, &QTimer::timeout, this, &GraphicsDialog::checkCollisions);
     collisionTimer->start(16);
@@ -137,6 +135,7 @@ void GraphicsDialog::checkCollisions() {
     bool collision = false; // This is used to tell that a player PREVIOUSLY was on a log
     for (auto &obstacle : obstacles)
     {
+        sendObstaclePositions();    // debug stuff
         // If player is colliding with something
         if (activePlayer->collidesWithItem(obstacle))
         {
